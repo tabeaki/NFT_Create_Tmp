1941208f6256dee6e0e72cf06fe5f342add0170b
takaaki tabei <43052438+tabeaki@users.noreply.github.com>
2022-12-29 05:08:12 +0900

    update

--- NFT.sol
+++ NFT.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.7.0 <0.9.0;
 //import "hardhat/console.sol"; // Hardhat console log
 
 import './ERC721A.sol';
-import "./node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "./Ownable.sol";
 
 /**
  * @title SmartGenerative
